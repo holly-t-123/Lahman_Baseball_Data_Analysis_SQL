a8719c7becf035c3c61a8f41afb84ea81bc18b23
toni-nss <enfl3141@gmail.com>
2021-06-08 11:35:13 -0500

    Q6 finished

--- baseball_toni.sql
+++ baseball_toni.sql
@@ -1,3 +1,8 @@
+--1
+--What range of years for baseball games played does the provided database cover?
+SELECT MIN(yearid),MAX(yearid)
+FROM appearances
+--1871~2016
 
 --2
 --Find the name and height of the shortest player in the database. --Edward Carl Gaedel / 43 / 1 game
@@ -13,14 +18,30 @@ ON a.teamid = t.teamid
 ORDER BY p.height
 
 
---3
---Find all players in the database who played at Vanderbilt University. 
---Create a list showing each player’s first and last names as well as the total salary they earned in the major leagues. 
---Sort this list in descending order by the total salary earned. 
---Which Vanderbilt player earned the most money in the majors?
+--6 :Jonathat Villar with 62 success
 
+--Find the player who had the most success stealing bases in 2016, 
+--where success is measured as the percentage of stolen base attempts which are successful. 
+--(A stolen base attempt results either in a stolen base or being caught stealing.) 
+--Consider only players who attempted at least 20 stolen bases.
 
+--sb: stolen bases
+--CS :caught stealing
 
---
-SELECT * 
-FROM appearances
+SELECT p.namefirst,
+       p.namelast,
+       yearid,
+       sb::decimal,
+	   cs::decimal,
+	   (sb+cs) as stolen_attemt,
+	   ROUND(sb/(sb+cs)::decimal,2) as success_rate
+	 
+	   
+FROM batting as b
+LEFT JOIN people as p
+ON b.playerid = p.playerid
+WHERE (sb+cs) >20
+AND yearid = 2016
+ORDER BY sb DESC
+
+	   
