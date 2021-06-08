c975f642f1612c0d82e6913b41151105051b37d5
holly-t-123 <holly.therrell@gmail.com>
2021-06-08 13:30:40 -0500

    made final answers file, finished question 9

--- holly_lahman_answers.sql
+++ holly_lahman_answers.sql
@@ -3,7 +3,7 @@
 SELECT 	MIN(yearid), 
 		MAX(yearid)
 FROM batting;
---covers 1971 through 2016 (or 2017? using halloffame)
+--covers 1871 through 2016 (or 2017? using halloffame)
 
 --Question 4 136815 rows in fielding, 11408383 total putouts in fielding
 SELECT SUM(PO)
@@ -33,6 +33,35 @@ WITH qualifying_managers AS (SELECT playerid,
 												AND lgid IN ('NL','AL')
 												GROUP BY playerid
 												HAVING COUNT(DISTINCT(lgid))=2) 
-							 )
+							 ),
+							 
+qm_with_names AS (	SELECT 	namefirst, 
+				  			namelast, 
+				  			qm.* 
+				  	FROM qualifying_managers AS qm 
+				  	LEFT JOIN people AS p 
+				  	USING(playerid)
+				 ),
+					
+qm_with_teamid AS (	SELECT 	DISTINCT q.*, 
+							m.teamid 
+					FROM qm_with_names AS q 
+					LEFT JOIN managers AS m 
+					USING(playerid)
+					LEFT JOIN teams AS t
+					USING(teamid)
+					WHERE m.yearid=q.yearid
+					AND m.lgid IN (SELECT lgid 
+								   FROM qm_with_names))
+								   
+SELECT 	qmt.namefirst || ' ' || qmt.namelast AS full_name,
+		t.name,
+		qmt.yearid,
+		awardid,
+		qmt.lgid
+FROM qm_with_teamid AS qmt 
+LEFT JOIN teams AS t 
+USING(teamid) 
+WHERE qmt.yearid = t.yearid 
+ORDER BY namefirst, yearid;
 
-SELECT * FROM teamsfranchises
