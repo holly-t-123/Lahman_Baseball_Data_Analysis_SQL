9ce08fff10ef9359e9fce81c60df0174a560b7fb
RyanButler47 <pokeyjoe47@gmail.com>
2021-06-09 11:25:15 -0500

    Q7 in progress

--- script_Q7_Ryan.sql
+++ script_Q7_Ryan.sql
@@ -46,12 +46,61 @@ ORDER BY w
 LIMIT 5;
 
 --7d.
-SELECT year, name, max(w)
+SELECT yearid, name, w
 FROM teams
+WHERE yearid >= '1970'
+GROUP BY yearid, name, w
+HAVING w =
+		(SELECT MAX(W)
+		 FROM teams
+		 WHERE wswin = 'Y');
 
 
+SELECT t1.name, t1.yearid, t1.w,
+		t2.yearid, t2.wswin
+FROM teams AS t1 INNER JOIN teams AS t2
+USING(yearid)
+WHERE t1.yearid >= '1970' 
+AND t2.yearid >= '1970'
+GROUP BY t1.name, t1.yearid, t1.w,
+		t2.yearid, t2.wswin
+HAVING MAX(t1.w) = t1.w
+AND T2.wswin = 'Y';
 
 
+SELECT name, yearid, w
+FROM teams
+WHERE yearid >= '1970'
+AND w IN
+	(SELECT MAX(w)
+	FROM teams
+	WHERE wswin = 'Y'
+	GROUP BY yearid)
+GROUP BY name, yearid, w;
+
+WITH max_wins_year AS
+(SELECT yearid, name, MAX(w) AS most_wins
+	 FROM teams
+	 WHERE yearid >= '1970'
+	 GROUP BY yearid, name
+	ORDER BY yearid DESC),
+wswinner AS (SELECT yearid, name
+	FROM teams
+	WHERE wswin = 'Y'
+		AND yearid >= '1970')
+
+SELECT t.name AS Team_name,
+		yearid AS Year,
+		w AS wins		
+FROM teams AS t
+INNER JOIN max_wins_year USING(yearid)
+INNER JOIN wswinner USING(yearid)
+WHERE yearid IN
+	(SELECT yearid
+	FROM max_wins_year)
+AND wswin = 'Y'
+GROUP BY yearid, t.name, w
+ORDER BY yearid;
 
 
 
