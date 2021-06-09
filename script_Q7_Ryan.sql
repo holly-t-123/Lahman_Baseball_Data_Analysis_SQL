110c50adf754344bd711c838ea9e6e522398b2e3
RyanButler47 <pokeyjoe47@gmail.com>
2021-06-09 14:53:19 -0500

    Q7 solution SQL Script Final

--- script_Q7_Ryan.sql
+++ script_Q7_Ryan.sql
@@ -46,62 +46,41 @@ ORDER BY w
 LIMIT 5;
 
 --7d.
-SELECT yearid, name, w
-FROM teams
-WHERE yearid >= '1970'
-GROUP BY yearid, name, w
-HAVING w =
-		(SELECT MAX(W)
-		 FROM teams
-		 WHERE wswin = 'Y');
+WITH max_wins AS 
+			(SELECT yearid,
+			MAX(w) AS w
+			FROM teams
+			WHERE yearid >= '1970'
+			GROUP BY yearid),
+max_wins_team AS 
+			(SELECT name, yearid
+			FROM max_wins LEFT JOIN teams USING(yearid)
+			WHERE teams.w = max_wins.w),
+wswinners AS (SELECT yearid, name, wswin
+				FROM teams
+				WHERE yearid >= '1970'
+				AND wswin = 'Y')
+SELECT * 
+FROM wswinners 
+FULL JOIN max_wins_team USING(yearid) 
+WHERE wswinners.name = max_wins_team.name
 
-
-SELECT t1.name, t1.yearid, t1.w,
-		t2.yearid, t2.wswin
-FROM teams AS t1 INNER JOIN teams AS t2
-USING(yearid)
-WHERE t1.yearid >= '1970' 
-AND t2.yearid >= '1970'
-GROUP BY t1.name, t1.yearid, t1.w,
-		t2.yearid, t2.wswin
-HAVING MAX(t1.w) = t1.w
-AND T2.wswin = 'Y';
-
-
-SELECT name, yearid, w
-FROM teams
-WHERE yearid >= '1970'
-AND w IN
-	(SELECT MAX(w)
-	FROM teams
-	WHERE wswin = 'Y'
-	GROUP BY yearid)
-GROUP BY name, yearid, w;
-
-WITH max_wins_year AS
-(SELECT yearid, name, MAX(w) AS most_wins
-	 FROM teams
-	 WHERE yearid >= '1970'
-	 GROUP BY yearid, name
-	ORDER BY yearid DESC),
-wswinner AS (SELECT yearid, name
-	FROM teams
-	WHERE wswin = 'Y'
-		AND yearid >= '1970')
-
-SELECT t.name AS Team_name,
-		yearid AS Year,
-		w AS wins		
-FROM teams AS t
-INNER JOIN max_wins_year USING(yearid)
-INNER JOIN wswinner USING(yearid)
-WHERE yearid IN
-	(SELECT yearid
-	FROM max_wins_year)
-AND wswin = 'Y'
-GROUP BY yearid, t.name, w
-ORDER BY yearid;
-
-
-
+--7e.
+WITH max_wins AS 
+			(SELECT yearid,
+			MAX(w) AS w
+			FROM teams
+			WHERE yearid >= '1970'
+			GROUP BY yearid),
+max_wins_team AS 
+			(SELECT name, yearid
+			FROM max_wins LEFT JOIN teams USING(yearid)
+			WHERE teams.w = max_wins.w),
+wswinners AS (SELECT yearid, name, wswin
+				FROM teams
+				WHERE yearid >= '1970'
+				AND wswin = 'Y')			 
+SELECT ROUND((COUNT(*)/(MAX(yearid)-MIN(yearid))::decimal*100),2) AS perc_time
+	FROM wswinners FULL JOIN max_wins_team USING(yearid) 
+	WHERE wswinners.name = max_wins_team.name;
 
